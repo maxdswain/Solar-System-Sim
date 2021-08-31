c3e32f25214d0e854d6b197e748b6c9ca19970ab
Max Swain <89113255+maxdswain@users.noreply.github.com>
2021-08-31 22:32:44 +0100

    compacting code

--- Planets.py
+++ Planets.py
@@ -25,12 +25,7 @@ def getPlanets(t=Time("2019-04-11 11:00:00", scale="tdb")):
     Bodies=[]
     for planet in [x for x in solar_system_ephemeris.bodies if x != "earth-moon-barycenter"]:
         pos, vel=get_body_barycentric_posvel(planet, t, ephemeris="jpl")
-        def metric(x):
-            if x==pos:
-                return "m"
-            elif x==vel:
-                return "m/s"
-        statevec=[x.xyz[i].to(metric(x)).value for x in [pos, vel] for i in range(3)]
+        statevec=[x.xyz[i].to("m").value if x==pos else x.xyz[i].to("m/s").value for x in [pos, vel] for i in range(3)]
         statevececl=mxvg(sxform("J2000", "ECLIPJ2000", t.jd), statevec, 6, 6)
         planet=Particle(
             position=np.array([statevececl[0], statevececl[1], statevececl[2]]),
